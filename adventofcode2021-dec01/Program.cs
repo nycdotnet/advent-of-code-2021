242a61c46eb338f8b035946d475b9a098ece5e6e
Steve Ognibene <steve.ognibene@outlook.com>
2021-12-04 14:07:06 -0500

    Working day 1 part 1

--- adventofcode2021-dec01/Program.cs
+++ adventofcode2021-dec01/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//var inputTextFileName = "example-input1.txt";
+var inputTextFileName = "myPuzzleInput1.txt";
+
+PrintDayOnePartOneAnswer(inputTextFileName);
+
+void PrintDayOnePartOneAnswer(string inputFileName)
+{
+    Console.WriteLine($"Solving Advent Of Code 2021 Day 1 Part One using {inputFileName}.");
+    var depthMeasurements = GetInputAsIntegerArray(inputFileName);
+    var depthIncreases = NumberOfTimesDepthIncreases(depthMeasurements);
+    Console.WriteLine($"The depth increases {depthIncreases} times.");
+}
+
+int NumberOfTimesDepthIncreases(int[] measurements)
+{
+    var result = 0;
+    for (var i = 0; i < measurements.Length - 1; i++)
+    {
+        if (measurements[i] < measurements[i + 1])
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+int[] GetInputAsIntegerArray(string fileName) => GetLines(fileName)
+    .AsParallel()
+    .Where(x => !string.IsNullOrWhiteSpace(x))
+    .Select(x => int.Parse(x))
+    .ToArray();
+
+string[] GetLines(string fileName) =>
+    File.ReadAllText(Path.Combine(ProjectFolder(), fileName))
+    .Split("\r\n");
+
+string ProjectFolder() => Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"));
