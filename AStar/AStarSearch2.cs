b4c783dcb87b9f512cf6810c1de99f8dd8dbbcec
Steve Ognibene <steve.ognibene@outlook.com>
2022-08-16 20:38:12 -0400

    Working implementation of AStar

---
+++ AStar/AStarSearch2.cs
@@ -0,0 +1,158 @@
+﻿using System.Xml.Linq;
+using static common.Utils;
+
+namespace AStar
+{
+    /// <summary>
+    /// Implements AStar where the cost is measured as a <see cref="short"/>.
+    /// </summary>
+    public class AStarSearch2<TLocationIdentifier> where TLocationIdentifier : notnull
+    {
+        // for the given key location identifier, returns the identifier of the previous location
+        // along the least expensive path.
+        public readonly Dictionary<TLocationIdentifier, TLocationIdentifier> cameFrom = new();
+
+        // the least expensive known cost to get to the given key location
+        public readonly Dictionary<TLocationIdentifier, short> bestKnownCostTo = new();
+
+        public AStarSearch2(IWeightedGraph<TLocationIdentifier> graph,
+            TLocationIdentifier start,
+            TLocationIdentifier goal,
+            Func<TLocationIdentifier, TLocationIdentifier, short> heuristic)
+        {
+            Start = start;
+            Goal = goal;
+
+            var frontier = new PriorityQueue<TLocationIdentifier, short>();
+            frontier.Enqueue(start, 0);
+
+            // we came from the start, so this is effectively the "identity element"
+            cameFrom[start] = start;
+            // there is zero cost to get here because this is the start.
+            bestKnownCostTo[start] = 0;
+
+            while (frontier.TryDequeue(out var current, out var _))
+            {
+                if (current.Equals(goal))
+                {
+                    Success = true;
+                    break;
+                }
+                var costToGetHere = bestKnownCostTo[current];
+                foreach (var next in graph.GetNeighbors(current))
+                {
+                    var newCost = (short)(costToGetHere + graph.Cost(current, next));
+                    if (!bestKnownCostTo.TryGetValue(next, out var previousBestCost) || previousBestCost > newCost)
+                    {
+                        // we have never been here, or the previous best cost was more expensive.
+                        bestKnownCostTo[next] = newCost;
+                        var priority = (short)(newCost + heuristic(next, goal));
+                        frontier.Enqueue(next, priority);
+                        cameFrom[next] = current;
+                    }
+                }
+            }
+        }
+
+        public TLocationIdentifier Start { get; }
+        public TLocationIdentifier Goal { get; }
+        public bool Success { get; private set; }
+
+        /// <summary>
+        /// returns the optimal path, navigating backward from the goal to the start.
+        /// </summary>
+        public IEnumerable<TLocationIdentifier> GetOptimalPath()
+        {
+            ThrowIfNotSuccess();
+
+            var path = new List<TLocationIdentifier>();
+
+            var current = Goal;
+            do
+            {
+                path.Add(current);
+                current = cameFrom[current];
+            } while (!current.Equals(Start));
+            return path;
+        }
+
+        public short GetOptimalPathCost()
+        {
+            ThrowIfNotSuccess();
+            return bestKnownCostTo[Goal];
+        }
+
+        private void ThrowIfNotSuccess()
+        {
+            if (!Success)
+            {
+                throw new Exception("No path found.");
+            }
+        }
+    }
+
+    public class Dec15Grid : IWeightedGraph<(int x, int y)>
+    {
+        private short[][] costs;
+        public Dec15Grid(string inputFileName, (int x, int y) start, (int x, int y) destination)
+        {
+            costs = GetMap(inputFileName);
+            var astar = new AStarSearch2<(int x, int y)>(this, start, destination, Cost);
+            OptimalPath = astar.GetOptimalPath().ToList();
+            OptimalPath.Reverse();
+            OptimalPathCost = astar.GetOptimalPathCost();
+
+        }
+
+        public List<(int x, int y)> OptimalPath { get; private set; }
+        public short OptimalPathCost { get; }
+
+        public short Cost((int x, int y) from, (int x, int y) to) => costs[to.y][to.x];
+
+        private bool InBounds((int x, int y) location) => location.x >= 0 && location.y >= 0 && location.y < costs.Length && location.x < costs[location.y].Length;
+
+        public IEnumerable<(int x, int y)> GetNeighbors((int x, int y) location)
+        {
+            // down
+            var test = (x: location.x, y: location.y + 1);
+            if (InBounds(test))
+            {
+                yield return test;
+            }
+
+            // up
+            test = (x: location.x, y: location.y - 1);
+            if (InBounds(test))
+            {
+                yield return test;
+            }
+
+            // left
+            test = (x: location.x - 1, y: location.y);
+            if (InBounds(test))
+            {
+                yield return test;
+            }
+
+            // right
+            test = (x: location.x + 1, y: location.y);
+            if (InBounds(test))
+            {
+                yield return test;
+            }
+        }
+
+        short[][] GetMap(string file) => GetLines(file)
+            .Select(line => line.ParseCharsToShorts().ToArray())
+            .ToArray();
+    }
+
+    public interface IWeightedGraph<TLocationIdentifier>
+    {
+        /// <summary>
+        /// Returns the cost to travel from location <paramref name="from"/> to <paramref name="to"/>.
+        /// </summary>
+        short Cost(TLocationIdentifier from, TLocationIdentifier to);
+        IEnumerable<TLocationIdentifier> GetNeighbors(TLocationIdentifier location);
+    }
+}
