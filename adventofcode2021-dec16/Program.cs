2f379be4bf64e8c65a0b9409e696f67e4beb3a29
Steve Ognibene <steve.ognibene@outlook.com>
2022-08-23 17:49:54 -0400

    more wip

--- adventofcode2021-dec16/Program.cs
+++ adventofcode2021-dec16/Program.cs
@@ -1,20 +1,11 @@
 ﻿using common;
 using common.BitArrayExtensionMethods;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Diagnostics;
-using System.Net.Sockets;
-using System.Text.RegularExpressions;
-using System.Threading;
-using static System.Collections.Specialized.BitVector32;
+
+// todo: read this http://graphics.stanford.edu/~seander/bithacks.html
 
 Console.WriteLine("Day 16: Packet Decoder");
-
-
-
-
 
 
 var example1 = "D2FE28";
@@ -23,6 +14,15 @@ var p = Packet.Parse(example1);
 Debug.Assert(p.Version == 6);
 Debug.Assert(p.PacketType == 4);
 Debug.Assert(p.Data.ToUInt32() == 2021);
+Debug.Assert(p.LengthTypeId == Packet.LengthType.NotSet);
+
+
+var operatorPacketExample = "38006F45291200";
+var op = Packet.Parse(operatorPacketExample);
+
+Debug.Assert(op.Version == 1);
+Debug.Assert(op.PacketType == 6);
+Debug.Assert(op.LengthTypeId == Packet.LengthType.TotalLengthInBits);
 
 //var first = p.Data[0].ToString("X");
 //var second = p.Data[1].ToString("X");
@@ -41,23 +41,37 @@ Debug.Assert(p.Data.ToUInt32() == 2021);
 
 public class BinaryNumber
 {
-    public BinaryNumber()
+    public void SetNibble(byte nibble)
     {
+        int offset = PrepareNextOffset();
+        var current = Data.Pop();
+        current |= nibble << offset;
+        Data.Push(current);
+        BitLength += 4;
     }
 
-    // For reference - I have validated that the nibbles are coming in correctly.
-    public void SetNibble(byte nibble)
+    private int PrepareNextOffset()
     {
         var nibbleOffset = BitLength % 32;
         if (nibbleOffset == 0)
         {
             Data.Push(0);
         }
-        var value = Data.Pop();
-        value |= nibble << nibbleOffset;
-        Data.Push(value);
+        return nibbleOffset;
+    }
 
-        BitLength += 4;
+    public void SetBit(bool value)
+    {
+        int offset = PrepareNextOffset();
+        var current = Data.Pop();
+        if (value)
+        {
+            current |= 1 << offset;
+        }
+        // we don't need an `else` here because this class initializes all the bits to 0.
+
+        Data.Push(current);
+        BitLength += 1;
     }
 
     public uint ToUInt32()
@@ -69,6 +83,7 @@ public class BinaryNumber
         uint result = 0;
 
         var nibbleCount = BitLength / 4;
+        var remainingBitCount = BitLength % 4;
         var writeIndex = BitLength - 4;
         for (var readNibbleIndex = 0; readNibbleIndex < nibbleCount; readNibbleIndex++)
         {
@@ -78,6 +93,19 @@ public class BinaryNumber
             //it would be interesting to do this without using plus and doing it purely with the bit operators
             result += (uint)(nibble << writeIndex >> (readNibbleIndex * 4));
             writeIndex -= 4;
+        }
+        if (remainingBitCount > 0)
+        {
+            result <<= remainingBitCount;
+            for (var readBitIndex = 0; readBitIndex < remainingBitCount; readBitIndex++)
+            {
+                var readMask = 0b1 << BitLength - 1 - readBitIndex;
+                var nibble = Data.Peek() & readMask;
+
+                //it would be interesting to do this without using plus and doing it purely with the bit operators
+                result += (uint)(nibble << writeIndex >> (readBitIndex * 4));
+                writeIndex -= 4;
+            }
         }
         return result;
     }
@@ -90,18 +118,6 @@ public class BinaryNumber
 
 public struct Packet
 {
-    //The BITS transmission contains a single packet at its outermost layer which itself contains
-    // many other packets. The hexadecimal representation of this packet might encode a few extra 0
-    // bits at the end; these are not part of the transmission and should be ignored.
-    // Every packet begins with a standard header: the first three bits encode the packet version,
-    // and the next three bits encode the packet type ID.These two values are numbers; all numbers
-    // encoded in any packet are represented as binary with the most significant bit first.
-    // For example, a version encoded as the binary sequence 100 represents the number 4.
-    // Packets with type ID 4 represent a literal value.  Literal value packets encode a single binary
-    // number.To do this, the binary number is padded with leading zeroes until its length is a multiple
-    // of four bits, and then it is broken into groups of four bits. Each group is prefixed by a 1 bit
-    // except the last group, which is prefixed by a 0 bit. These groups of five bits immediately
-    // follow the packet header.
     public static Packet Parse(ReadOnlySpan<char> s)
     {
         var ba = new BitArray(ReadBytesFromHexChars(s));
@@ -109,9 +125,34 @@ public struct Packet
         
         result.Version = ba.GetByte(VERSION_INDEX, VERSION_LENGTH);        
         result.PacketType = ba.GetByte(PACKET_TYPE_INDEX, PACKET_TYPE_LENGTH);
-        result.Data = new();
 
         if (result.PacketType == PACKET_TYPE_LITERAL_VALUE)
+        {
+            ParseLiteralPacket();
+        }
+        else
+        {
+            ParseOperatorPacket();
+        }
+        
+        return result;
+
+        void ParseOperatorPacket()
+        {
+            var ltid = ba.GetByte(FIRST_DATA_INDEX, LENGTH_TYPE_ID_LENGTH);
+            result.LengthTypeId = ltid == 0b1 ? LengthType.NumberOfSubPackets : LengthType.TotalLengthInBits;
+
+            if (result.LengthTypeId == LengthType.TotalLengthInBits)
+            {
+
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        void ParseLiteralPacket()
         {
             var readIndex = FIRST_DATA_INDEX;
             var readMore = true;
@@ -127,8 +168,14 @@ public struct Packet
                 readIndex += DATA_LENGTH;
             }
         }
-        
-        return result;
+    }
+
+    public Packet()
+    {
+        LengthTypeId = LengthType.NotSet;
+        Data = new();
+        Version = 0;
+        PacketType = 0;
     }
 
     private const int READ_MORE_MASK = 0b10000;
@@ -137,6 +184,7 @@ public struct Packet
     private const int VERSION_LENGTH = 3;
     private const int PACKET_TYPE_INDEX = 3;
     private const int PACKET_TYPE_LENGTH = 3;
+    private const int LENGTH_TYPE_ID_LENGTH = 1;
     private const int FIRST_DATA_INDEX = 6;
     private const int DATA_LENGTH = 5;
     private const int PACKET_TYPE_LITERAL_VALUE = 4;
@@ -144,6 +192,13 @@ public struct Packet
     public byte Version { get; private set; }
     public byte PacketType { get; private set; }
     public BinaryNumber Data { get; private set; }
+    public LengthType LengthTypeId { get; private set; }
+    public enum LengthType
+    {
+        TotalLengthInBits = 0,
+        NumberOfSubPackets = 1,
+        NotSet = -1
+    }
 
     private static byte[] ReadBytesFromHexChars(ReadOnlySpan<char> s)
     {
